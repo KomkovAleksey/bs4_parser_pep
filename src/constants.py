db5222c073fd7223241b55c3455f827e2c2b8860
Алексей Комков <ale45883412@yandex.ru>
2024-01-11 00:03:03 +0700

    Добавил PEP_DOC_URL, LOG_FORMAT, DT_FORMAT, EXPECTED_STATUS.

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,19 @@
 from pathlib import Path
 
 MAIN_DOC_URL = 'https://docs.python.org/3/'
-BASE_DIR = Path(__file__).parent 
+PEP_DOC_URL = 'https://peps.python.org/'
+BASE_DIR = Path(__file__).parent
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
+DT_FORMAT = '%d.%m.%Y %H:%M:%S'
+
+EXPECTED_STATUS = {
+    'A': ('Active', 'Accepted'),
+    'D': ('Deferred',),
+    'F': ('Final',),
+    'P': ('Provisional',),
+    'R': ('Rejected',),
+    'S': ('Superseded',),
+    'W': ('Withdrawn',),
+    '': ('Draft', 'Active'),
+}
